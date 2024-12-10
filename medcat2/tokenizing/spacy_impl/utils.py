cb5b48dc4a8f7e659343c8ead9b967c0056f276c
mart-r <mart.ratas@gmail.com>
2024-12-10 16:04:45 +0000

    Add debug output

--- medcat2/tokenizing/spacy_impl/utils.py
+++ medcat2/tokenizing/spacy_impl/utils.py
@@ -35,4 +35,6 @@ def ensure_spacy_model(model_name: str) -> None:
     cmd = f"{sys.executable} -m spacy download {model_name}"
     logger.info("Installing the spacy model %s using the CLI command "
                 "'%s'", model_name, cmd)
+    print("RUN/cmd[START]")
     subprocess.run(cmd.split(" "), check=True)
+    print("RUN/cmd[END]")
