271e3306f37342f48f6f58afb4626262d104b983
mart-r <mart.ratas@gmail.com>
2025-01-16 11:14:41 +0000

    CU-8697g0vcc: Add arbitrary init kwargs to deserialisation; add model load path to methods for getting component init arguments

--- tests/components/addons/test_addons.py
+++ tests/components/addons/test_addons.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 
 from medcat2.components.addons import addons
 
@@ -31,13 +31,13 @@ class FakeAddonWithInit:
         return doc
 
     @classmethod
-    def get_init_args(cls, tokenizer: BaseTokenizer, cdb: CDB, vocab: Vocab
-                      ) -> list[Any]:
+    def get_init_args(cls, tokenizer: BaseTokenizer, cdb: CDB, vocab: Vocab,
+                      model_load_path: Optional[str]) -> list[Any]:
         return [tokenizer, cdb]
 
     @classmethod
-    def get_init_kwargs(cls, tokenizer: BaseTokenizer, cdb: CDB, vocab: Vocab
-                        ) -> dict[str, Any]:
+    def get_init_kwargs(cls, tokenizer: BaseTokenizer, cdb: CDB, vocab: Vocab,
+                        model_load_path: Optional[str]) -> dict[str, Any]:
         return {}
 
 
