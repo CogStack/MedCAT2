af35ee13b9466be79a35e3f512ddb1cf5aacd07e
mart-r <mart.ratas@gmail.com>
2025-01-20 16:47:03 +0000

    CU-8697kbhmw: Update missing naming changes

--- medcat2/trainer.py
+++ medcat2/trainer.py
@@ -15,7 +15,7 @@ from medcat2.data.mctexport import (MedCATTrainerExport,
                                     MedCATTrainerExportDocument)
 from medcat2.preprocessors.cleaners import prepare_name, NameDescriptor
 from medcat2.components.types import CoreComponentType, TrainableComponent
-from medcat2.platform.pipeline import Pipeline
+from medcat2.pipeline.pipeline import Pipeline
 
 
 logger = logging.getLogger(__name__)
